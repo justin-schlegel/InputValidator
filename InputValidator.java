936a2e62c51e12f2d9337d0415bf18e889bec4dc
justin-schlegel <justin.schlegel@live.com>
2018-01-12 09:32:38 -0500

    Update InputValidator.java

--- InputValidator.java
+++ InputValidator.java
@@ -3,53 +3,33 @@
 //SWENG586_Assignment1
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
-public class InputValidator {
+public class Test {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
 		String userInput = null;
-		Boolean isInputValid = false;
-		
 		Scanner scanner = new Scanner(System.in);
-		
-		do {
+		boolean running = true;
+		while (running) {
 			System.out.println("---------------------------------------------------------------------------------------------------");
-			System.out.println("|  Enter some text and let it be a string with only alphanumric characters; minimum 8 in length!  |");
+			System.out.println("| Type 'quit' to exit                                                                             |");
+			System.out.println("---------------------------------------------------------------------------------------------------");
+			System.out.println("---------------------------------------------------------------------------------------------------");	
+			System.out.println("|  Enter some text and let it be a string with only alphanumeric characters; minimum 8 in length! |");
 			System.out.println("---------------------------------------------------------------------------------------------------");
 			userInput = scanner.nextLine();
-						
-			Pattern alphaNumRegex = Pattern.compile("[^a-zA-Z0-9]");
-			boolean hasSpecialChar = alphaNumRegex.matcher(userInput).find();
-		
-			while (hasSpecialChar == true) {
-				System.out.println("Now you've gone and done it all wrong! Alphanumeric only!");
-				System.out.println("");
-				System.out.println("---------------------------------------------------------------------------------------------------");
-				System.out.println("|  Enter something and let it be a string with only alphanumric characters; minimum 8 in length!  |");
-				System.out.println("---------------------------------------------------------------------------------------------------");
-				userInput = scanner.nextLine();
-				
-				Pattern alphaNumRegex2 = Pattern.compile("[^a-zA-Z0-9]");
-				boolean hasSpecialChar2 = alphaNumRegex2.matcher(userInput).find();
-				
-				if (hasSpecialChar2 == false) {
-					hasSpecialChar = false;
-				}
+
+			if ("quit".equalsIgnoreCase(userInput)) {
+				System.out.println("Goodbye!");
+				scanner.close();
+				System.exit(0);
 			}
-			if (userInput.length() < 8 ) {
-				System.out.println("Too Short!");
-				System.out.println("");
-			} else {
-				isInputValid = true;	
-			}
-		} while (isInputValid == false); 
-	
-		System.out.println("You entered " + '"'+userInput+'"' + ", an excellent choice.");
-		scanner.close();
+			
+			boolean isInputValid = userInput.matches("[a-zA-Z0-9]{8,}$");
+			
+			if (isInputValid == true) System.out.println("You entered " + '"'+userInput+'"' + ", an excellent choice.");
+			else System.out.println("Now you've gone and done it all wrong! Enter 8 or more alphanumeric characters!");
+		}
 	}
 }
-
-
